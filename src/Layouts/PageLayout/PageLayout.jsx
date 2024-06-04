0c550c58d281e40f0adc711f660930e74bbe1705
Ayesha Siddiqa <19ayeshasidd@gmail.com>
2024-06-04 07:54:21 +0530

    Social media application built using react and firebase, completely responsive and includes almost all expected features

---
+++ src/Layouts/PageLayout/PageLayout.jsx
@@ -0,0 +1,50 @@
+import { Flex, Box, Spinner } from "@chakra-ui/react";
+
+import { useLocation } from "react-router-dom";
+
+import SideBar from "../../components/SideBar/SideBar";
+import { auth } from "../../firebase/firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+import Navbar from "../../components/Navbar/Navbar";
+
+const PageLayout = ({ children }) => {
+  const { pathname } = useLocation();
+  const [user, loading] = useAuthState(auth);
+  const canRenderSidebar = pathname !== "/auth" && user;
+  const canRenderNavbar = !user && !loading && pathname !== "/auth";
+
+  const checkingUserIsAuth = !user && loading;
+  if (checkingUserIsAuth) return <PageLayoutSpinner />;
+
+  return (
+    <Flex flexDir={canRenderNavbar ? "column" : "row"}>
+      {/* sidebar on left/*/}
+      {canRenderSidebar ? (
+        <Box w={{ base: "70px", md: "240px" }}>
+          <SideBar />
+        </Box>
+      ) : null}
+      {/* navbar */}
+      {canRenderNavbar ? <Navbar /> : null}
+      {/* content on right*/}
+      <Box flex={1} w={{ base: "calc(100% - 70px)", md: "calc(100% - 240px)" }}>
+        {children}
+      </Box>
+    </Flex>
+  );
+};
+
+export default PageLayout;
+
+const PageLayoutSpinner = () => {
+  return (
+    <Flex
+      flexDir="column"
+      h="100vh"
+      alignItems="center"
+      justifyContent="center"
+    >
+      <Spinner size="xl" />
+    </Flex>
+  );
+};
